25ce35c5ccda47c5e00ef37503e9f1a18f0612d9
Gianluca <gl.galassini@gmail.com>
2022-09-07 18:18:49 +0200

    copy button

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ Vue.createApp({
     },
     computed: {
         box() {
-            return {transform: `
-                perspective(${this.perspective}px)
-                rotateX(${this.rotateX}deg)
-                rotateY(${this.rotateY}deg)
-                rotateZ(${this.rotateZ}deg)
-                `
+            return {transform:`
+            perspective(${this.perspective}px)
+            rotateX(${this.rotateX}deg)
+            rotateY(${this.rotateY}deg)
+            rotateZ(${this.rotateZ}deg)
+            `
             }
         }
     },
@@ -24,6 +24,12 @@ Vue.createApp({
             this.rotateX = 0,
             this.rotateY = 0,
             this.rotateZ =0
+        },
+        async copy() {
+            let text = `transform: ${this.box.transform}`;
+            await navigator.clipboard.writeText(text);
+
+            alert('CSS Copied to Clipboard');
         }
     }
 }).mount('#app');
